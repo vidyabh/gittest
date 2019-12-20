69cd1aa9ddcf869119809a1235a84401196b07d6
vidyabh <59045559+vidyabh@users.noreply.github.com>
2019-12-20 13:00:57 +0530

    master

--- src/test.java
+++ src/test.java
@@ -3,6 +3,6 @@ public class test {
 public static void main(String[] args) {
 	String abc="";
 	System.out.println("jhjhhhj");
-	System.out.println("i am from sub class");
+	System.out.println("i am from master class");
 }
 }
